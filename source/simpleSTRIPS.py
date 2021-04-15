ece15d71693c01cdddd0e4fc0a49905a4a57dfb6
Jordan Faas-Bush <jmanfb@gmail.com>
2021-04-15 15:46:40 -0400

    Added A* forwards planner

--- source/simpleSTRIPS.py
+++ source/simpleSTRIPS.py
@@ -39,6 +39,9 @@ Basically if the action has a change that influences something that is not satis
 the current unstatisfied world state because they aren't useful towards making your plan actually a thing. Is it possible to counter-plan against another AI using reverse planning? Maybe not.
 """
 
+import heapq
+from time import process_time
+
 
 # use BFS (prioritizing by fewest actions) for forward planning or use distance from goal using a simple measurement. Currently BFS makes much shorter paths
 USE_BFS = True
@@ -82,15 +85,18 @@ class StripsWorld:
 	def __ge__(self, other):
 		# this is used to compare states. If this state is greater than or equal to the other state then this state contains that state
 		# (i.e. if this is a goal state you want this to be greater than the current state)
-		for our_key in self.state:
+		for our_key in other.state:
 			# For now, let's require that the other state have keys that are equal to this and not bother with actual numbers since __ge__ doesn't support that anyways
 			# we could do things like require >10 money but that seems a pain since it requires defining how to compare each key of the world state and what is better than
 			# what, which may change from planner to planner
-			if (our_key not in other.state):
+			if (our_key not in self.state):
 				return False
 			elif self.state[our_key] != other.state[our_key]:
 				return False
 		return True
+
+	def __lt__(self, other):
+		return not self.__ge__(other)
 
 	def __hash__(self):
 		return self.simple_hash()
@@ -237,9 +243,82 @@ class SimpleSTRIPSPlanner:
 		self.startingstate = startingstate
 		self.actions = actions
 
-	def forward_plan(self, max_depth = 50000):
+	def forwards_a_star_plan(self, max_depth = 50000, return_incomplete_path = True):
+		# this is forwards planning atm but using A* not BFS or greedy Dijkstra's
+		if (self.startingstate >= self.goalstate):
+			return True, [], self.startingstate # then we're already there!
+
+		frontier = []
+		# heapq.heapify(frontier)
+		heapq.heappush(frontier, (0, self.startingstate)) # 0 cost to go to the starting state!
+
+		starting_hash = hash(self.startingstate)
+		costs = {starting_hash:(0, starting_hash, None)} # cost/visited dictionary!
+		# hash of state : (cost to get here, hash of state to get here, action taken to get here)
+
+		tried_plans = 1
+		i = 0
+
+		current_state = self.startingstate
+
+		while len(frontier) > 0:
+			# try out an element!
+			i += 1
+			current_loc = heapq.heappop(frontier)
+			# print("Working on " + str(current_loc))
+			original_cost = current_loc[0] # get the weight from the tuple
+			current_state = current_loc[1] # get the world state
+			original_hash = hash(current_state)
+
+			# then check if we made it to the goal!
+			if current_state >= self.goalstate:
+				# then we've made it!
+				break
+
+			# if not then figure out what actions we can do!
+			possible_actions = [action for action in self.actions if action.can_be_done(current_state)]
+			for a in possible_actions:
+				# figure out their cost and add them to the heap!
+				tried_plans += 1
+				clone = current_state.clone()
+				a.do_action(clone)
+				h = hash(clone)
+				current_cost = original_cost + a.cost
+				if (h not in costs) or (costs[h][0] > current_cost):
+					if h in costs:
+						print("Overwrote cost!")
+					# only add it to the list of costs if it doesn't already exist or if this is a better way to do it!
+					# then add it to the list of costs!
+					# we should also figure out the heuristic of the cost for A*! That makes sense!
+					heuristic = current_state.compare_states(self.goalstate, True)
+					costs[h] = (current_cost, original_hash, a)
+					# now add this new state to the frontier!
+					heapq.heappush(frontier, (current_cost + heuristic, clone))
+			if i > max_depth:
+				print("Canceled due to max tries")
+				break # didn't make it. :(
+
+		print("NUMBER OF TRIED PLANS: " + str(tried_plans))
+		print("NUMBER OF PLANS ON FRONTIER: " + str(len(frontier)))
+
+		if current_state >= self.goalstate or return_incomplete_path:
+			# we made it! Figure out the list of actions to get there
+			current_hash = hash(current_state)
+			actions = []
+			while current_hash != starting_hash:
+				# go backwards in the list to figure out where we came from!
+				prev_step = costs[current_hash]
+				actions += [prev_step[2]]
+				current_hash = prev_step[1]
+			actions.reverse()
+			return current_state >= self.goalstate, actions, current_state
+
+		return False, [], self.startingstate # couldn't make it! Probably should return the best path but whatever works.
+
+
+	def forwards_greedy_plan(self, max_depth = 50000):
 		# forward plan, returning a list of actions that it would take
-		if (self.goalstate >= self.startingstate):
+		if (self.startingstate >= self.goalstate):
 			return True, [], self.startingstate
 		actions = []
 		frontier = [(self.startingstate.clone(), self.goalstate.compare_states(self.startingstate), [])]
@@ -262,28 +341,60 @@ class SimpleSTRIPSPlanner:
 					visited.add(h)
 					action_list_clone = [a for a in current_state[2]] + [action]
 					distance = self.goalstate.compare_states(clone) # the distance from the goal state
-					if USE_BFS:
-						frontier += [[clone, distance, action_list_clone]]
+					# this version sorts by distance than # actions
+					num_new_actions = len(action_list_clone)
+					for i, v in enumerate(frontier):
+						if v[1] >= distance:
+							# then compare number of actions
+							j = i
+							while j < len(frontier) and frontier[j][1] == distance:
+								# compare the number of actions so you can now prioritize those with fewer actions
+								if len(frontier[j][2]) > num_new_actions:
+									# then insert yourself at index j
+									break
+								j += 1
+							# insert the clone here
+							frontier.insert(j, [clone, distance, action_list_clone])
+							break # you've found where you should insert the new one so exit this loop
 					else:
-						# this version sorts by distance than # actions
-						num_new_actions = len(action_list_clone)
-						for i, v in enumerate(frontier):
-							if v[1] >= distance:
-								# then compare number of actions
-								j = i
-								while j < len(frontier) and frontier[j][1] == distance:
-									# compare the number of actions so you can now prioritize those with fewer actions
-									if len(frontier[j][2]) > num_new_actions:
-										# then insert yourself at index j
-										break
-									j += 1
-								# insert the clone here
-								frontier.insert(j, [clone, distance, action_list_clone])
-								break # you've found where you should insert the new one so exit this loop
-						else:
-							# it didn't find any smaller than the new distance so place yourself at the back
-							frontier += [[clone, distance, action_list_clone]]
-					if (self.goalstate >= clone):
+						# it didn't find any smaller than the new distance so place yourself at the back
+						frontier += [[clone, distance, action_list_clone]]
+					if (clone >= self.goalstate):
+						print("NUMBER OF TRIED PLANS: " + str(tried_plans))
+						print("NUMBER OF PLANS ON FRONTIER: " + str(len(frontier)))
+						return True, action_list_clone, clone
+				# otherwise it's already visted that state and tried everything from there. This should prevent obvious infinite loops
+			if i > max_depth:
+				break # didn't make it. :(
+		return False, [], self.startingstate # first is whether or not you got to the goal the second is the list of actions to take
+
+	def forwards_bfs_plan(self, max_depth = 50000):
+		# forward plan, returning a list of actions that it would take
+		if (self.startingstate >= self.goalstate):
+			return True, [], self.startingstate
+		actions = []
+		frontier = [(self.startingstate.clone(), self.goalstate.compare_states(self.startingstate), [])]
+		i = 0
+		tried_plans = 1
+		visited = {hash(self.startingstate)} # this is annoying. Perhaps a hash would work best rather than comparing an ever increasing number of world states? For now just limit the loops
+		# the frontier is the list of worldstates, difference from the goal state, and actions to get there. Eventually I should sort this based on difference from the goalstate
+		while len(frontier) > 0:
+			i += 1
+			current_state = frontier[0]
+			frontier = frontier[1:]
+			possible_actions = [action for action in self.actions if action.can_be_done(current_state[0])]
+			for action in possible_actions:
+				# do the action and add the new action to the frontier. For now just do a BFS. This will likely need to change for performance's sake
+				clone = current_state[0].clone()
+				tried_plans += 1 # keeping track of how many paths it walked down
+				action.do_action(clone)
+				h = hash(clone)
+				if h not in visited:
+					visited.add(h)
+					action_list_clone = [a for a in current_state[2]] + [action]
+					distance = self.goalstate.compare_states(clone) # the distance from the goal state
+					frontier += [[clone, distance, action_list_clone]]
+					if (clone >= self.goalstate):
 						print("NUMBER OF TRIED PLANS: " + str(tried_plans))
 						print("NUMBER OF PLANS ON FRONTIER: " + str(len(frontier)))
 						return True, action_list_clone, clone
@@ -296,13 +407,16 @@ class SimpleSTRIPSPlanner:
 		out = "Plan by " + str(self.name) + "\n"
 		out += "Starting State " + str(self.startingstate) + "\n"
 		testWorldState = self.startingstate.clone()
+		cost = 0
 		for action in plan:
+			cost += action.cost
 			if (not action.can_be_done(testWorldState)):
 				out += "OH DEAR THIS ACTION CAN'T BE DONE:\n"
 			action.apply(testWorldState)
 			out += str(action) + " : " + str(testWorldState) + "\n"
 		#out += "\n".join([str(action) for action in plan]) + "\n"
 		out += "Hopefully ending at goal state: " + str(self.goalstate)
+		out += "\nCost: " + str(cost)
 		return out
 
 
@@ -315,7 +429,7 @@ if __name__ == "__main__":
 	goalKeys = {"moneyInInventory":20}
 	goalWorld = StripsWorld(goalKeys)
 	print(goalWorld)
-	print("Has goal already succeded? " + str(goalWorld >= startingWorld))
+	print("Has goal already succeded? " + str(startingWorld >= goalWorld))
 
 	changes = {"setlocation_woods":SimpleSTRIPSChange("Go to woods", "characterLocation", "=", "woods"), \
 				"setlocation_hut":SimpleSTRIPSChange("Go to hut", "characterLocation", "=", "hut"),\
@@ -323,37 +437,58 @@ if __name__ == "__main__":
 
 	prereqs = {"isat_woods":STRIPSEqualPrereq("Is at woods", "characterLocation", "woods"),\
 				"isat_hut":STRIPSEqualPrereq("Is at hut", "characterLocation",  "hut"),\
-				"isat_store":STRIPSEqualPrereq("Is at store", "characterLocation",  "store")}
+				"isat_store":STRIPSEqualPrereq("Is at store", "characterLocation",  "store"),
+				"has<5_logs":SimpleSTRIPSPrereq("Has < 5 logs", "logsInInventory", "<", 5)}
 
 	actions = []
 	# SimpleSTRIPSAction(name cost prereqs changes)
 	# SimpleSTRIPSChange(name key action value)
 	# SimpleSTRIPSPrereq(name key comparison value)
 	#actions.append(SimpleSTRIPSAction("Free Money", 0, [], [SimpleSTRIPSChange("Increase money by one", "moneyInInventory", "+=", 1)]))
-	actions.append(SimpleSTRIPSAction("Throw Away Money", 0, [], [SimpleSTRIPSChange("Decrease money by one", "moneyInInventory", "-=", 1)]))
-	actions.append(SimpleSTRIPSAction("Hut To Woods", 0, [prereqs["isat_hut"]], [changes["setlocation_woods"]]))
-	actions.append(SimpleSTRIPSAction("Woods To Hut", 0, [prereqs["isat_woods"]], [changes["setlocation_hut"]]))
-	actions.append(SimpleSTRIPSAction("Hut To Store", 0, [prereqs["isat_hut"]], [changes["setlocation_store"]]))
-	actions.append(SimpleSTRIPSAction("Store To Hut", 0, [prereqs["isat_store"]], [changes["setlocation_hut"]]))
-	actions.append(SimpleSTRIPSAction("Gather Sticks", 0, [prereqs["isat_woods"]], [SimpleSTRIPSChange("Add Stick to inventory", "sticksInInventory", "+=", 1)]))
-	actions.append(SimpleSTRIPSAction("Sell stick", 0,
+	actions.append(SimpleSTRIPSAction("Throw Away Money", 1, [], [SimpleSTRIPSChange("Decrease money by one", "moneyInInventory", "-=", 1)]))
+	actions.append(SimpleSTRIPSAction("Hut To Woods", 1, [prereqs["isat_hut"]], [changes["setlocation_woods"]]))
+	actions.append(SimpleSTRIPSAction("Woods To Hut", 1, [prereqs["isat_woods"]], [changes["setlocation_hut"]]))
+	actions.append(SimpleSTRIPSAction("Hut To Store", 1, [prereqs["isat_hut"]], [changes["setlocation_store"]]))
+	actions.append(SimpleSTRIPSAction("Store To Hut", 1, [prereqs["isat_store"]], [changes["setlocation_hut"]]))
+	actions.append(SimpleSTRIPSAction("Gather Sticks", 1, [prereqs["isat_woods"]], [SimpleSTRIPSChange("Add Stick to inventory", "sticksInInventory", "+=", 1)]))
+	actions.append(SimpleSTRIPSAction("Sell stick", 1,
 			[prereqs["isat_store"], SimpleSTRIPSPrereq("Has a stick", "sticksInInventory", ">", 0)], \
 			[SimpleSTRIPSChange("Remove Stick from inventory", "sticksInInventory", "-=", 1), SimpleSTRIPSChange("Add .5 money to inventory", "moneyInInventory", "+=", .5)]))
-	actions.append(SimpleSTRIPSAction("Sell log", 0,
+	actions.append(SimpleSTRIPSAction("Sell log", 1,
 			[prereqs["isat_store"], SimpleSTRIPSPrereq("Has a log", "logsInInventory", ">", 0)], \
 			[SimpleSTRIPSChange("Remove log from inventory", "logsInInventory", "-=", 1),SimpleSTRIPSChange("Add 1 money to inventory", "moneyInInventory", "+=", 1)]))
-	actions.append(SimpleSTRIPSAction("Buy axe", 0,
+	actions.append(SimpleSTRIPSAction("Buy axe", 1,
 			[prereqs["isat_store"], SimpleSTRIPSPrereq("Has 2 money", "moneyInInventory", ">=", 2)], \
 			[SimpleSTRIPSChange("Remove money from inventory", "moneyInInventory", "-=", 2), SimpleSTRIPSChange("Add axe to inventory", "hasAxe", "=", True)]))
-	actions.append(SimpleSTRIPSAction("Chop down tree", 0,
-			[prereqs["isat_woods"], SimpleSTRIPSPrereq("Has axe", "hasAxe", "==", True)], \
-			[SimpleSTRIPSChange("Add 2 logs to inventory", "logsInInventory", "+=", 5)]))
-	#actions.append(SimpleSTRIPSAction("Magically get money", 0, [], [SimpleSTRIPSChange("Add 1 money to inventory", "moneyInInventory", "+=", 1)]))
+	actions.append(SimpleSTRIPSAction("Chop down tree", 1,
+			[
+			prereqs["isat_woods"],
+			# prereqs["has<5_logs"],
+			SimpleSTRIPSPrereq("Has axe", "hasAxe", "==", True)], \
+			[SimpleSTRIPSChange("Add 5 logs to inventory", "logsInInventory", "+=", 5)]))
+	# actions.append(SimpleSTRIPSAction("Magically get money", 1, [], [SimpleSTRIPSChange("Add 1 money to inventory", "moneyInInventory", "+=", 1)]))
 
 	planner = SimpleSTRIPSPlanner("Woodcutter", startingWorld, goalWorld, actions)
-	success, plan, final_state = planner.forward_plan(100000)
+
+	start_time = process_time()
+
+
+	# success, plan, final_state = planner.forwards_greedy_plan(100000)
+	# success, plan, final_state = planner.forwards_bfs_plan(100000)
+	success, plan, final_state = planner.forwards_a_star_plan(100000)
+
+	time_taken = process_time() - start_time
+
 	print(planner.plan_string(plan))
 	print("Final State: " + str(final_state))
 	print("Did it make it? " + str(success))
+	print("Num Actions", len(plan))
+	print("Time taken:", time_taken)
 	#print("")
 	#print("\n\n".join([s.full_str() for s in plan]))
+
+
+
+	# I think the only way to fix the bad planning is to plan backwards, because now if you disable chopping down trees (or selling logs/buying an
+	# an axe) then it'll incorrectly sell fewer sticks early and have to go back and gather more sticks.
+	# I think that backwards planning would prevent that hopefully! Perhaps I should implement that at some point.
